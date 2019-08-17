6b26c5709912fe46a538b8d34da082851c205087
vviilloovv <vviilloovv@gmail.com>
2019-08-17 22:58:22 +0900

    rubocop指摘修正

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,7 +13,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2019_08_17_120745) do
-
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.text "memo"
@@ -20,5 +21,4 @@ ActiveRecord::Schema.define(version: 2019_08_17_120745) do
     t.string "author"
     t.string "picture"
   end
-
 end
