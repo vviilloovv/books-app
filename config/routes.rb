62aa18a883a1bed8b23d7c0abf9a491b19f9c7a6
vviilloovv <vviilloovv@gmail.com>
2019-08-21 22:29:35 +0900

    ルーティングに言語を指定できるよう設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@
 
 Rails.application.routes.draw do
   resources :books
+
+  scope "(:locale)" do
+    resources :products
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
