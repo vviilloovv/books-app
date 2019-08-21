eb192bcceb55c0bb918b14038a07a17bed99a7a8
vviilloovv <vviilloovv@gmail.com>
2019-08-21 22:59:13 +0900

    ルーティング設定が誤っていたため修正

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   resources :books
 
   scope "(:locale)" do
-    resources :products
+    resources :books
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
