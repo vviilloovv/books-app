92631ce991f7502f706a1ed385a916e1a929cd2c
vviilloovv <vviilloovv@gmail.com>
2019-08-27 00:16:16 +0900

    ルート設定 #5

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   scope "(:locale)" do
     resources :books
   end
+  root to: "books#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
