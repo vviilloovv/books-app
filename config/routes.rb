5887db2cc9a374cdbaf0d211c9040b60d1c43e5a
vviilloovv <vviilloovv@gmail.com>
2019-08-27 01:02:13 +0900

    ルーティングを国際化対応 #5

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users
   scope "(:locale)" do
     resources :books
+    devise_for :users
   end
   root to: "books#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
