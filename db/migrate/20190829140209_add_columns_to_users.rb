4cc78088dddf9c66dd3024237c068580cb47fa28
vviilloovv <vviilloovv@gmail.com>
2019-08-29 23:41:14 +0900

    ユーザーテーブルに名前、郵便番号、住所、自己紹介を追加 #5

---
+++ db/migrate/20190829140209_add_columns_to_users.rb
@@ -0,0 +1,8 @@
+class AddColumnsToUsers < ActiveRecord::Migration[6.0]
+  def change
+    add_column :users, :name, :string, null: false, default: "名無しさん"
+    add_column :users, :postal_code, :integer
+    add_column :users, :address, :string
+    add_column :users, :profile, :text
+  end
+end
