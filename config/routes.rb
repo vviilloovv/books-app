51515f79c6ef091aa61ffbee2903339048ecc0c5
vviilloovv <vviilloovv@gmail.com>
2019-09-11 00:35:36 +0900

    deviseのコントローラを編集できるように追加 #5

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@
 Rails.application.routes.draw do
   scope "(:locale)" do
     resources :books
-    devise_for :users
+    devise_for :users, controllers: {
+      registrations: "users/registrations",
+      sessions: "users/sessions",
+    }
   end
   root to: "books#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
