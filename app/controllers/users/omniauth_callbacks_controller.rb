2a91fe548145dac4afb1bb95d0edd59797828c5a
vviilloovv <vviilloovv@gmail.com>
2019-11-16 23:27:11 +0900

    コールバック処理をコントローラに追加 #7

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,6 +1,11 @@
 # frozen_string_literal: true
 
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  def github
+    @user = User.from_omniauth(request.env["omniauth.auth"])
+    sign_in_and_redirect @user, event: :authentication
+    set_flash_message(:notice, :success, kind: "Github") if is_navigational_format?
+  end
   # You should configure your model like this:
   # devise :omniauthable, omniauth_providers: [:twitter]
 
--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
+  def build_resource(hash = {})
+    hash[:uid] = User.create_unique_string
+    super
+  end
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
