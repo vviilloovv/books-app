e62146f88cfb54ce3d717e4573e1853e0914ea8d
vviilloovv <vviilloovv@gmail.com>
2020-03-25 17:38:33 +0900

    #13 ユーザー検索を`before_action`に変更

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,22 +1,22 @@
 # frozen_string_literal: true
 
 class RelationshipsController < ApplicationController
+  before_action :set_user
+
   def create
-    user = set_user
-    following = current_user.follow(user)
+    following = current_user.follow(@user)
     notice_message = following.save ? "フォロー成功" : "フォロー失敗"
     redirect_back(fallback_location: users_path, notice: notice_message)
   end
 
   def destroy
-    user = set_user
-    following = current_user.unfollow(user)
+    following = current_user.unfollow(@user)
     notice_message = following.destroy ? "フォロー解除成功" : "フォロー解除失敗"
     redirect_back(fallback_location: users_path, notice: notice_message)
   end
 
   private
     def set_user
-      User.find(params[:relationship][:follow_id])
+      @user = User.find(params[:relationship][:follow_id])
     end
 end
