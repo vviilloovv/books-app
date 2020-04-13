acac945f0d8b9c5e2bbd40a56c1be23498486b9c
vviilloovv <vviilloovv@gmail.com>
2020-04-13 15:04:25 +0900

    #21 コメント編集機能追加

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,13 +2,17 @@
 
 class CommentsController < ApplicationController
   before_action :login_required
-  before_action :set_comment, only: [:update, :destroy]
+  before_action :set_comment, only: [:edit, :update, :destroy]
   before_action :check_user, only: [:update, :destroy]
 
   # GET /comments
   # GET /comments.json
   def index
     @comments = current_user.comments.order(created_at: "DESC").page(params[:page])
+  end
+
+  # GET /comments/1/edit
+  def edit
   end
 
   # POST /comments
@@ -21,7 +25,7 @@ class CommentsController < ApplicationController
         format.html { redirect_back(fallback_location: comments_url, notice: "Comment was successfully created.") }
         format.json { render :index, status: :created, location: @comment }
       else
-        format.html { render :back }
+        format.html { redirect_to comments_url, notice: "Failed create comment." }
         format.json { render json: @comment.errors, status: :unprocessable_entity }
       end
     end
@@ -32,7 +36,7 @@ class CommentsController < ApplicationController
   def update
     respond_to do |format|
       if @comment.update(comment_params)
-        format.html { redirect_back(fallback_location: comments_url, notice: "Comment was successfully updated.") }
+        format.html { redirect_to @comment.commentable, notice: "Comment was successfully updated." }
         format.json { render :index, status: :ok, location: @comment }
       else
         format.html { render :back }
@@ -59,7 +63,7 @@ class CommentsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def comment_params
-      params.require(:comment).permit(:content)
+      params.require(:comment).permit(:content, :commentable_type, :commentable_id)
     end
 
     def check_user
