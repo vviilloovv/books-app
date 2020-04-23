a5f6a5cf0c44d79200b3a47292d407a96af68a57
vviilloovv <vviilloovv@gmail.com>
2020-04-23 12:03:32 +0900

    #24 bookのshowテストを追加

--- test/system/books_test.rb
+++ test/system/books_test.rb
@@ -5,11 +5,11 @@ require "application_system_test_case"
 class BooksTest < ApplicationSystemTestCase
   def setup
     @alice = users(:alice)
+    sign_in @alice
     @book = books(:one)
   end
 
   test "一覧表示" do
-    sign_in @alice
     visit books_url
 
     assert_selector "h1", text: "Listing books"
@@ -17,8 +17,13 @@ class BooksTest < ApplicationSystemTestCase
     assert_text "Bob Book"
   end
 
+  test "詳細表示" do
+    visit @book
+
+    assert_text "Alice in Wonderland"
+  end
+
   test "新規作成" do
-    sign_in @alice
     visit new_book_path
 
     fill_in "タイトル", with: "Neo Alice"
@@ -33,9 +38,7 @@ class BooksTest < ApplicationSystemTestCase
   end
 
   test "更新" do
-    sign_in @alice
-    visit books_url
-    click_on "Edit", match: :first
+    visit edit_book_path(@book)
 
     fill_in "タイトル", with: "不思議の国のアリス"
     fill_in "概要", with: "鏡の国"
@@ -43,13 +46,9 @@ class BooksTest < ApplicationSystemTestCase
     click_button "更新する"
 
     assert_text "Book was successfully updated."
-
-    click_on "Back"
-    assert_no_text "Alice in Wonderland"
   end
 
   test "削除" do
-    sign_in @alice
     visit books_url
     accept_alert do
       click_on "Destroy", match: :first
@@ -58,13 +57,4 @@ class BooksTest < ApplicationSystemTestCase
     assert_text "Book was successfully destroyed."
     assert_no_text "Alice in Wonderland"
   end
-#
-#  test "destroying a Book" do
-#    visit books_url
-#    page.accept_confirm do
-#      click_on "Destroy", match: :first
-#    end
-#
-#    assert_text "Book was successfully destroyed"
-#  end
 end
